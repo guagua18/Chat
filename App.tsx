6191147987620b66b43d210763a4a187fbe4cf63
laq <1991182634@qq.com>
2026-03-31 18:00:30 +0800

    初始化配置

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import './global.css';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text className='text-3xl text-blue-500'>Hello World!123132</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
