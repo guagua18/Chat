840e26515406b16d332e8827a0b625317679ae8f
laq <1991182634@qq.com>
2026-04-14 16:47:18 +0800

    创建频道

--- src/app/(drawer)/(home)/new/chat.tsx
+++ src/app/(drawer)/(home)/new/chat.tsx
@@ -4,7 +4,10 @@ import { User } from "@/types";
 import { useUser } from "@clerk/clerk-expo";
 import { useMutation } from "@tanstack/react-query";
 import { channel } from "diagnostics_channel";
+import { router } from "expo-router";
 import { View, Text } from "react-native"
+
+// TODO: 检查是否已存在与点击用户的直接沟通渠道
 
 export default function NewChat() {
 
@@ -13,19 +16,39 @@ export default function NewChat() {
 
   const createChannel = useMutation({
     mutationFn: async (clickedUser: User) => {
-      console.log("createChannel")
-      const { data } = await supabase.from('channels').insert({
+      // 1.Create a channel(if it does't exist)
+      const { data: channel } = await supabase.from('channels').insert({
         type: 'direct'
+      }).throwOnError().select('*').single();
+
+      if (!channel) {
+        throw new Error('通道为空');
+      }
+
+      // 2. Add user to the channel
+      await supabase.from('channel_users').insert({
+        channel_id: channel.id,
+        user_id: clickedUser.id
       }).throwOnError();
 
-      return data;
+      // 3. Add myself to the channel
+      await supabase.from('channel_users').insert({
+        channel_id: channel.id,
+        user_id: user!.id
+      }).throwOnError();
+
+      return channel;
+    },
+    onSuccess(newChannel) {
+      // 4. Redirect to the channel page
+      router.back()
+      router.push(`/channel/${newChannel.id}`)
     }
   })
 
-  // 1.Create a channel(if it does't exist)
-  // 2. Add user to the channel
-  // 3. Add myself to the channel
-  // 4. Redirect to the channel page
+
+
+
 
   const handleUserPress = (user: User) => {
     console.log("handleUserPress:", user.id)
